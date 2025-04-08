55cd7d90ad2ce02084610d4e1f2dbe9b459cfee5
martin <maa.velazquez@alumno.um.edu.ar>
2025-04-07 22:50:09 -0300

    commit conversor romanos to decimal

---
+++ test/test_roman_conversor.py
@@ -0,0 +1,25 @@
+import unittest
+
+from src.roman_converter import decimal_to_roman
+
+class TestRomanConverter(unittest.TestCase):
+    def test_basic_numbers(self):
+        self.assertEqual(decimal_to_roman(1), "I")
+        self.assertEqual(decimal_to_roman(5), "V")
+        self.assertEqual(decimal_to_roman(10), "X")
+
+    def test_subtraction_rules(self):
+        self.assertEqual(decimal_to_roman(4), "IV")
+        self.assertEqual(decimal_to_roman(9), "IX")
+        self.assertEqual(decimal_to_roman(40), "XL")
+        self.assertEqual(decimal_to_roman(90), "XC")
+
+    def test_complex_numbers(self):
+        self.assertEqual(decimal_to_roman(49), "XLIX")
+        self.assertEqual(decimal_to_roman(99), "XCIX")
+        self.assertEqual(decimal_to_roman(499), "CDXCIX")
+        self.assertEqual(decimal_to_roman(999), "CMXCIX")
+        self.assertEqual(decimal_to_roman(3999), "MMMCMXCIX")
+
+if __name__ == '__main__':
+    unittest.main()
